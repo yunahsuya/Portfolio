81301a916295d05824821b66dde3565281a703e0
Yuna <yunahsuya@gmail.com>
2025-09-11 17:10:19 +0800

    編輯文章內容

--- src/App.vue
+++ src/App.vue
@@ -1361,18 +1361,29 @@
             <div class="p-6 flex flex-col flex-grow">
               <h3 class="text-xl font-bold text-gray-900 mb-2">環保餐廳 LineBot 機器人</h3>
               <p class="text-gray-600 mb-4 flex-grow">
-                以 Bootstrap 5
-                製作的響應式展示網站。能自動依照螢幕大小調整版面，模擬一般商業網站的基本排版。
+                整合環境部開放資料 API，透過定位自動推薦最近的環保認證餐廳，使用 Node.js 後端與
+                Haversine 公式計算距離，提供餐廳資訊與一鍵導航。
               </p>
               <div class="flex flex-wrap gap-2 mb-6">
-                <span class="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-sm">HTML5</span>
-                <span class="px-3 py-1 bg-green-100 text-green-700 rounded-full text-sm">CSS3</span>
-                <span class="px-3 py-1 bg-purple-100 text-purple-700 rounded-full text-sm"
-                  >JavaScript</span
-                >
                 <span class="px-3 py-1 bg-orange-100 text-orange-700 rounded-full text-sm"
-                  >Bootstrap</span
+                  >Node.js</span
                 >
+
+                <span class="px-3 py-1 bg-purple-100 text-purple-700 rounded-full text-sm"
+                  >Express</span
+                >
+
+                <span class="px-3 py-1 bg-green-100 text-green-700 rounded-full text-sm"
+                  >Axios
+                </span>
+
+                <span class="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-sm"
+                  >LINE Bot SDK</span
+                >
+
+                <span class="px-3 py-1 bg-orange-100 text-orange-700 rounded-full text-sm"
+                  >Nodemon
+                </span>
               </div>
 
               <div class="flex justify-between items-center mt-auto">
@@ -1507,10 +1518,9 @@
               />
             </div>
             <div class="p-6 flex flex-col flex-grow">
-              <h3 class="text-xl font-bold text-gray-900 mb-2">文章介紹</h3>
+              <h3 class="text-xl font-bold text-gray-900 mb-2">知識與文章分享</h3>
               <p class="text-gray-600 mb-4 flex-grow">
-                以 Bootstrap 5
-                製作的響應式展示網站。能自動依照螢幕大小調整版面，模擬一般商業網站的基本排版。
+                我喜歡將專案經驗、學習心得和生活觀察整理成文章分享，不僅能幫助自己梳理思路，也希望透過文字傳遞價值給讀者。
               </p>
               <div class="flex flex-wrap gap-2 mb-6">
                 <span class="px-3 py-1 bg-blue-100 text-blue-700 rounded-full text-sm">HTML</span>
